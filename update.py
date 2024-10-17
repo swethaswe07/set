45e6484ddd214b6c942db483c6010bbf2ef20c49
Swetha P V <swethapv03@gmail.com>
2024-10-17 13:57:37 +0530

    Update update.py

--- update.py
+++ update.py
@@ -7,17 +7,13 @@ Sample Output:
 1 2 3 4 5
 
 '''
-# Read the first set of values and convert to a set of integers
-set1 = set(map(int, input().split()))
 
-# Read the second set of values and convert to a set of integers
+
+set1 = set(map(int, input().split()))
 set2 = set(map(int, input().split()))
 
-# Update set1 with values from set2
 set1.update(set2)
 
-# Convert the updated set to a sorted list
 sorted_combined = sorted(set1)
 
-# Print the updated values with space separation
 print(" ".join(map(str, sorted_combined)))
